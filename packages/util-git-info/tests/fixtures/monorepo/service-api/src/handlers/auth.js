4e2ae367c7c1f8d6be7260cc2aff84a2799f8a44
DavidWells <hello@davidwells.io>
2025-11-17 19:11:10 -0800

    add sub files

--- packages/util-git-info/tests/fixtures/monorepo/service-api/src/handlers/auth.js
+++ packages/util-git-info/tests/fixtures/monorepo/service-api/src/handlers/auth.js
@@ -1,10 +1,11 @@
 // Authentication Lambda handlers
+import authDep from './utils/auth-dep'
 
 export async function authenticate(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'authenticate function called'
+      message: 'authenticate function called ' + authDep
     }),
   }
 }
---
+++ packages/util-git-info/tests/fixtures/monorepo/service-api/src/handlers/utils/auth-dep.js
@@ -0,0 +1,3 @@
+const authDep = 'foo'
+
+export default authDep
